25c02f680a9a875e82afab9449311f822f811700
jpribeiro7 <josepribeiro@ua.pt>
2021-10-12 01:03:40 +0100

    fix switch button

--- src/components/toggle/index.tsx
+++ src/components/toggle/index.tsx
@@ -1,20 +1,32 @@
-import React from "react";
+import React,
+       {useState} from "react";
 import { Container, 
         ToggleSelector,
         ToggleLable } from "../toggle/styles";
 
 
- const Toggle: React.FC = () => (
-    <Container>
-        <ToggleLable>Light</ToggleLable>
-        <ToggleSelector checked 
-                uncheckedIcon={false}
-                checkedIcon={false} 
-                onChange={() => console.log('Trocou')}
-                />
-        
-        <ToggleLable>Dark</ToggleLable>
-    </Container>
-)
+
+const Toggle: React.FC = () => {
+    
+    const [switchChecked, setStateChecked] = useState(false);
+    const handleChange = (state:boolean) => {
+        setStateChecked(state);
+    };
+
+    return (
+        <Container>
+            <ToggleLable>Light</ToggleLable>
+            <ToggleSelector 
+                    checked={switchChecked}
+                    uncheckedIcon={false}
+                    checkedIcon={false} 
+                    onChange={handleChange}
+                    
+            />
+            
+            <ToggleLable>Dark</ToggleLable>
+        </Container>
+    )
+}
 export default Toggle;
 
